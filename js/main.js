4fcff0dca0a3bc433fd5121afcf48f2f50eb72f1
максим корнеев <Maxsim119@yandex.ru>
2019-10-07 14:43:35 +0300

    discr not finished

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//auth
+//---------------------------------------auth---------------------
 let correctPassword = 'admin';
 let correctLogin = 'admin';
 
@@ -15,4 +15,34 @@ authButton.addEventListener('click', (event) => {
         alert('Вы вошли в систему.')
     } else alert('Введен неверный логин или пароль.')
 });
-//-------------------------------------------------------
+//---------------------------------------math-form------------------------------
+
+let coeffA = document.querySelector('.form-math__coeff-A').value;
+let coeffB = document.querySelector('.form-math__coeff-B').value;
+let coeffC = document.querySelector('.form-math__coeff-C').value;
+
+let buttonResult = document.querySelector('.form-math_button');
+
+buttonResult.addEventListener('click', function () {
+    let quadraticEquation = (a, b, c) => {
+        if (a == 0) {
+            return false
+        }
+        let res = {};
+        let D = b * b - 4 * a * c;
+        console.log('D = ' + D);
+        if (D < 0) {
+            return false
+        }
+        res['discriminant'] = D;
+        if (D == 0) {
+            res['quadratic roots'] = (-b + Math.sqrt(D)) / (2 * a);
+        } else if (D > 0) {
+            let tmp = [];
+            tmp.push((-b + Math.sqrt(D)) / (2 * a));
+            tmp.push((-b - Math.sqrt(D)) / (2 * a));
+            res['quadratic roots'] = tmp;
+        }
+        return res;
+    }
+});
