25cc8c5123521a2fd7ac80655b5e641a77be1182
BarryWin <43175007+BarryWin@users.noreply.github.com>
2019-10-25 18:43:23 +0300

    table generation

--- js/shuman.js
+++ js/shuman.js
@@ -4,27 +4,30 @@ let bodyTable = table.querySelector('tbody');
 
 testAmount.addEventListener('input', () => {
 
-    let tableChildren = bodyTable.childNodes;
+    let tableChildren = Object.values(bodyTable.children);
 
     console.log(tableChildren);
 
     tableChildren[0].innerHTML = '<td class="model-shuman__column"></td>';
-    tableChildren[2].innerHTML = '<td class="model-shuman__column">Кол-во ошибок</td>';
-    tableChildren[4].innerHTML = '<td class="model-shuman__column">Время тестирования</td>';
+    tableChildren[1].innerHTML = '<td class="model-shuman__column">Кол-во ошибок</td>';
+    tableChildren[2].innerHTML = '<td class="model-shuman__column">Время тестирования</td>';
 
     tableChildren.forEach((row) => {
 
-        // console.log(row);
+        console.log(row);
 
         for(let i = 0; i< testAmount.value; i++) {
 
             if (row.className === 'model-shuman__row model-shuman__test-number') {
-                row.innerHTML +=`<td class="model-shuman__column">${ i+1}</td>`;
+
+                row.innerHTML +=`<td class="model-shuman__column"> ${i+1} </td>`;
+
             } else {
+
                 row.innerHTML += `<td class="model-shuman__column"><input type="number"></td>`
             }
         }
     })
 
 
-})
+});
