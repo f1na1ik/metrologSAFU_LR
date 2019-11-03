458eef0b24b6a3bbfdcceebe8d72506a9108b7f2
BarryWin <43175007+BarryWin@users.noreply.github.com>
2019-11-03 20:24:19 +0300

    model shuman is ready

--- js/shuman.js
+++ js/shuman.js
@@ -6,9 +6,9 @@ testAmount.addEventListener('change', () => {
 
 	if (testAmount.value <= 20 && testAmount.value >= 0) {
 
-        testAmount.style.color = '';
+		testAmount.style.color = '';
 
-        let tableChildren = Object.values(bodyTable.children);
+		let tableChildren = Object.values(bodyTable.children);
 
 		console.log(tableChildren);
 
@@ -35,12 +35,151 @@ testAmount.addEventListener('change', () => {
 
 	} else {
 
-	    testAmount.style.color = 'red';
-    }
+		testAmount.style.color = 'red';
+	}
 });
 
 let countButton = document.querySelector('.model-shuman__button');
+let file = document.querySelector('input[type=file]');
 
 countButton.addEventListener('click', (e) => {
+
 	e.preventDefault();
-})
+
+	let kol = 0;
+
+	let a = +document.querySelector('#inputA').value,
+		ak = 0,
+		at = 0;
+
+	let b = +document.querySelector('#inputB').value,
+		bk = 0,
+		bt = 0;
+
+	let qk = 0;
+	let t = +document.querySelector('#workTime').value;
+
+	let tableChildren = Object.values(bodyTable.children);
+
+	let columnsAmount = tableChildren[0].querySelectorAll('td');
+	let columnsErrors = tableChildren[1].querySelectorAll('input[type=number]');
+	let columnsTime = tableChildren[2].querySelectorAll('input[type=number]');
+
+	for(let i = 0; i <= (a-1); i++) {
+
+		ak += +columnsErrors[i].value;
+		at += +columnsTime[i].value;
+	}
+
+	for(let j = a; j <= (b-1); j++) {
+
+		bk += +columnsErrors[j].value;
+		bt += +columnsTime[j].value;
+	}
+
+	for(let q = 0; q <= (testAmount.value); q++) {
+
+		qk += +columnsAmount[q].innerText;
+	}
+
+	file.click(qk);
+
+	file.onchange = () => {
+
+		let fileReader = new FileReader();
+
+		fileReader.readAsText(file.files[0]);
+
+		fileReader.onload = () => {
+
+			let codeArray = fileReader.result.split(' ');
+
+			codeArray.forEach(codeItem => {
+
+				switch (codeItem) {
+
+					case 'if':
+						kol += 1;
+						break;
+
+					case 'else':
+						kol += 1;
+						break;
+
+					case '=':
+						kol += 1;
+						break;
+
+					case '==':
+						kol += 1;
+						break;
+
+					case '===':
+						kol += 1;
+						break;
+
+					case '>':
+						kol += 1;
+						break;
+
+					case '<':
+						kol += 1;
+						break;
+
+					case '>=':
+						kol += 1;
+						break;
+
+					case '<=':
+						kol += 1;
+						break;
+
+					case '!=':
+						kol += 1;
+						break;
+
+					case '!':
+						kol += 1;
+						break;
+
+					case '*':
+						kol += 1;
+						break;
+
+					case '+':
+						kol += 1;
+						break;
+
+					case '-':
+						kol += 1;
+						break;
+
+					case '/':
+						kol += 1;
+						break;
+				}
+
+			});
+
+			let statements = document.querySelector('.model-shuman__statements');
+
+			statements.innerHTML = `Количество операторов ${kol}`;
+
+			let ea = ak/kol;
+			let eb = bk/kol;
+			let ia = ak/kol;
+			let ib = bk/bt;
+			let Et = (kol * ((ia/ib) * ea-eb)) / ((ia/ib) - 1);
+			let C = ia / (Et/kol - ea);
+			let R = Math.round((Math.exp(C * ((Et/kol) - (qk/kol)) * t) * 100));
+
+			alert(R);
+
+			console.log(qk);
+			console.log(kol);
+			console.log(codeArray);
+		}
+
+	};
+
+});
